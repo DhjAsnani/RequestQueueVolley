6f0274206ea26a659f37fde260f3670ad72b7434
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-23 15:08:42 +0530

    Request Queue Added

--- app/src/main/java/com/example/gohan/volleyrequestqdemo/MainActivity.java
+++ app/src/main/java/com/example/gohan/volleyrequestqdemo/MainActivity.java
@@ -2,15 +2,20 @@ package com.example.gohan.volleyrequestqdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.android.volley.Cache;
 import com.android.volley.Network;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
 import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
 
 public class MainActivity extends AppCompatActivity {
     Button btn;
@@ -27,5 +32,26 @@ public class MainActivity extends AppCompatActivity {
         Cache cache = new DiskBasedCache(getCacheDir(),1024*1024);
         // For n/w transaction
         Network network = new BasicNetwork(new HurlStack());
+        requestQueue = new RequestQueue(cache,network);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        textView.setText(response);
+                        requestQueue.stop();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText("Error....");
+                        error.printStackTrace();
+                        requestQueue.stop();
+                    }
+                });
+                requestQueue.add(stringRequest);
+            }
+        });
     }
 }
