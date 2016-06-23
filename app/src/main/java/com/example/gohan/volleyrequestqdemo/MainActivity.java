613f98f151931fab6ef34e064490097b0d077a29
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-23 14:58:59 +0530

    Cache Mem and Network Transaction Added

--- app/src/main/java/com/example/gohan/volleyrequestqdemo/MainActivity.java
+++ app/src/main/java/com/example/gohan/volleyrequestqdemo/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.gohan.volleyrequestqdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    TextView textView;
+    String server_url = "http://192.168.26.11:8080/Learn/volley/greetings.php";
+    RequestQueue requestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.ser_btn);
+        textView = (TextView) findViewById(R.id.ser_txt);
+        // For message Cache
+        Cache cache = new DiskBasedCache(getCacheDir(),1024*1024);
+        // For n/w transaction
+        Network network = new BasicNetwork(new HurlStack());
     }
 }
